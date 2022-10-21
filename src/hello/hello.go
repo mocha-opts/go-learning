8473309e4d74bc73b0fab9850255bd801ebcf75d
coconut <1424392205@qq.com>
2022-10-21 17:07:33 +0800

    🍲feat: 匿名变量

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main()  {
-	fmt.Println("Hello World!")
+	x, _ := foo()
+	_, y := foo()
+	fmt.Println("x=", x)
+	fmt.Println("y=", y)
+	
+	n := 10;
+	m := 200
+	fmt.Println("Hello World!",n,m)
 }
+func foo()(int,string){
+	return 10, "hh"
+}
+//匿名变量不占用命名空间，不会分配内存，所以匿名变量之间不存在重复声明。 
+//函数外的每个语句都必须以关键字开始（var、const、func等）
+//:=不能使用在函数外。
+//_多用于占位，表示忽略值。
